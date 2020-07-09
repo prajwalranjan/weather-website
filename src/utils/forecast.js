ae584e2b77fd07470f2bc9df497af1ba860291b7
prajwalranjan <prajwal.ranjan@outlook.com>
2020-07-09 21:31:18 +0530

    Updated forecast page

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,22 +9,9 @@ const forecast = (longitude, latitude, callback) => {
         } else if(response.body.error) {
             callback(chalk.red("Unable to find location..."), undefined);
         } else{
-            callback(undefined, response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature + " degrees out. It feels like " + response.body.current.feelslike + " out.");
+            callback(undefined, response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature + " degrees out. It feels like " + response.body.current.feelslike + " out. The humidity is " + response.body.current.humidity + "%.");
         } 
     })
 }
 
 module.exports = forecast;
-
-//weatherstack key=fefbe5785a358404739b276040467396
-// const url = 'http://api.weatherstack.com/current?access_key=fefbe5785a358404739b276040467396&query=12.9716,77.5946';
-
-// request({url: url, json: true}, (error, response)=> {
-//     if (error){
-//         console.log(chalk.red("Unable to connect to Weather Service..."));
-//     } else if(response.body.error) {
-//         console.log(chalk.red("Unable to find location..."));
-//     } else{
-//         console.log(response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature + " degrees out. It feels like " + response.body.current.feelslike + " out.");
-//     }
-// })
